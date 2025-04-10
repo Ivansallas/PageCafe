17a558a035cfe0f026cf6af66169c9fec3a4f023
Ivansallas <kiss_ivan@hotmail.com>
2025-04-09 22:29:59 -0300

    termindo agora

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ contactForm.addEventListener("submit", (e) => {
     submitButton.style.backgroundColor = "#6F4E37";
     
     // Redirecionar para a home após envio
-    window.location.href = "https://ivansallas.github.io/PageCafe/";
+    window.location.href = "#home";
   }, 1000);
 });
 
